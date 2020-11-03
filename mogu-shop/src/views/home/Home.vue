1f7e69b7d1b52d16a9b7233b6d798eed955f3229
Hurst <304090717@qq.com>
2020-11-03 16:29:41 +0800

    完成轮播图的展示

--- mogu-shop/src/views/home/Home.vue
+++ mogu-shop/src/views/home/Home.vue
@@ -1,20 +1,50 @@
 <template>
     <div>
+        <!-- 导航栏 -->
         <nav-bar class="home-nav">
             <div slot="center">蘑菇街</div>
         </nav-bar>
+        <!-- 轮播图 -->
+        <home-swiper :banners="banners"></home-swiper>
+
     </div>
 </template>
 
 <script>
-    import NavBar from '@/components/common/navbar/NavBar'
+    //导入页面组件
+    import HomeSwiper from "./childComps/HomeSwiper";
+
+    //导入公共文件
+    import NavBar from "@/components/common/navbar/NavBar";
+
+    //导入函数 
+    import { getHomeMultidata } from "network/home";
+   
 
     export default {
-        name: 'Home',
+        name: "Home",
         components: {
-            NavBar
-        }
-    }
+            NavBar,
+            HomeSwiper
+        },
+        data() {
+            return {
+                banners: [],
+                recommends: [],
+            };
+        },
+        created() {
+            this.getHomeMultidata();
+        },
+        methods: {
+            getHomeMultidata() {
+                getHomeMultidata().then((res) => {
+                    this.banners = res.data.banner.list;
+                    this.recommends = res.data.recommend.list;
+                });
+            },
+        },
+    };
 </script>
 
 <style scoped>
